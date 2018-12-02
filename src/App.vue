94808a44ddae07ba7f4ee62a8ece4b05a4094d42
SteveCruise <tgbhuytgbhuyt@126.com>
2018-12-02 14:52:15 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="上海伦之信息科技有限公司"/>
+    <div class="footer">
+      <a href="http://www.miitbeian.gov.cn">沪ICP备18044920号</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 60px;*/
+  }
+  .footer{
+    height: 100px;
+    width: 100%;
+    /*background-color: #ddd;*/
+    position: fixed;
+    bottom: 0;
+  }
+</style>
