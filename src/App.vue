b8ed9ca39597e57b06d2f265bf2cc436a2318b4c
SteveCruise <tgbhuytgbhuyt@126.com>
2020-03-16 15:12:00 +0800

    v1.0.1 fix .dockerignore bug

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,6 @@
     <template>
       <router-view></router-view>
     </template>
-
-
-    <!--<HelloWorld msg="上海伦之信息科技有限公司"/>-->
-    <!--<el-tabs type="border-card" class="container">-->
-      <!--<el-tab-pane label="关于我们">-->
-        <!--<el-collapse v-model="activeName" accordion>-->
-          <!--<el-collapse-item title="公司介绍" name="1">-->
-            <!--<div>上海伦之信息科技有限公司成立于2018年05月19日，坐落于上海浦东张江科技园区，主营手机网络游戏的开发和运营。未来计划更名为上海勿忘我信息科技有限公司。</div>-->
-          <!--</el-collapse-item>-->
-          <!--<el-collapse-item title="品牌介绍" name="2">-->
-            <!--<div>“勿忘我游戏”以全新品牌闪亮登场，以“不氪不肝还好玩”的理念，耕耘公平竞技细分游戏市场，打造公平竞技游戏品牌，让广大玩家放心玩游戏。</div>-->
-          <!--</el-collapse-item>-->
-          <!--<el-collapse-item title="主要产品介绍" name="3">-->
-            <!--<div>勿忘我游戏首推“英雄竞技场”手机网络游戏，该游戏是一款3D战术竞技游戏合集，全国同服，Android和iOS同服。英雄竞技场手游已开发完毕，待公司各项手续齐全，将上架Taptap和App Store，供广大玩家下载。</div>-->
-          <!--</el-collapse-item>-->
-          <!--<el-collapse-item title="联系我们" name="4">-->
-            <!--<div>客户服务：kf@mail.wwwgames.cn</div>-->
-            <!--<div>商务合作：business@mail.wwwgames.cn</div>-->
-          <!--</el-collapse-item>-->
-        <!--</el-collapse>-->
-      <!--</el-tab-pane>-->
-      <!--<el-tab-pane label="招聘英才">暂无招聘信息</el-tab-pane>-->
-    <!--</el-tabs>-->
-
-    <!--<div class="footer">-->
-
-    <!--</div>-->
   </div>
 </template>
 
